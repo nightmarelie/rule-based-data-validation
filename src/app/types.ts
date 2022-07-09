827ef6a99dbf3f1e195b1201a9fe170bef0b7ce3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-09 14:03:32 -0600

    feat: add app form type

---
+++ src/app/types.ts
@@ -0,0 +1,25 @@
+type ApplicantName = string;
+type PhoneNumber = string;
+type EmailAddress = string;
+type BirthDate = DateString;
+type UserPhoto = Image;
+
+type KnownSpecialty = "engineer" | "scientist" | "psychologist";
+type UnknownSpecialty = string;
+type ExperienceYears = NumberLike;
+
+type Password = string;
+
+export type ApplicationForm = {
+  name: ApplicantName;
+  phone: PhoneNumber;
+  email: EmailAddress;
+  birthDate: BirthDate;
+  photo: UserPhoto;
+
+  specialty: KnownSpecialty;
+  customSpecialty: UnknownSpecialty;
+  experience: ExperienceYears;
+
+  password: Password;
+};
