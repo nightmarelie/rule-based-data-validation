38f1825154533a15c5341a2b5a3cf5e86a4d7dd4
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-09 14:42:30 -0600

    feat: add email and phone validation

--- src/app/validation.ts
+++ src/app/validation.ts
@@ -2,3 +2,9 @@ import type { ApplicationFormData, ApplicationFormValidationRule } from "./types
 import { exists } from "./utils";
 
 export const validateName: ApplicationFormValidationRule = ({ name }) => exists(name);
+
+export const validateEmail: ApplicationFormValidationRule = ({ email }) =>
+  email.includes("@") && email.includes(".");
+
+export const validatePhone: ApplicationFormValidationRule = ({ phone }) =>
+  phone.startsWith("+") && phone.search(/[^\d\s\-\(\)\+]/g) < 0;
